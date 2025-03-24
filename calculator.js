cd7412d65baf92f368a035f4adb8308205c3cd15
eattie <fillet@gmail.com>
2025-03-24 01:48:04 -0700

    basic css html progress

---
+++ calculator.js
@@ -0,0 +1,9 @@
+currentNumber = 0
+
+function addNum() {
+    // figure out which button the user clicked
+    // multiply currentNumber * 10 + the digit they clicked
+    // currNum = 0, they click 5, output = 5
+    // currNum = 1, they click 1, output = 11
+    // currNum = 23, they click 4, output 234
+}
